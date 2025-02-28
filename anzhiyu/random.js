9e76043a670e130f83581c972952becb6c5b802a
kpinquan <kpinquan@gmail.com>
2025-03-01 00:01:42 +0800

    Site updated: 2025-03-01 00:01:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/31/測試/","2025/01/31/hello-world/"];function toRandomPost(){
+var posts=["2025/01/31/hello-world/","2025/01/31/測試/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
